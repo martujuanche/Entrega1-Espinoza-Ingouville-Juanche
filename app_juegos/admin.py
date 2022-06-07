c7f8cb16761723b2228634c3125b7a6523ffd6cf
mateo ingouville <mateoingouville@gmail.com>
2022-06-06 22:58:16 -0300

    imagenes por admin + cambios varios

--- app_juegos/admin.py
+++ app_juegos/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from app_juegos.models import Juguete, Sucursal, Empleados
 
-admin.site.register(Juguete)
-admin.site.register(Sucursal)
-admin.site.register(Empleados)
+@admin.register(Juguete)
+class ProductsAmin(admin.ModelAdmin):
+    list_display = ['name']
+@admin.register(Sucursal)
+class SucursalAmin(admin.ModelAdmin):
+    list_display = ['adress']
+@admin.register(Empleados)
+class Empleados(admin.ModelAdmin):
+    list_display = ['nombre']
--- app_juegos/models.py
+++ app_juegos/models.py
@@ -4,6 +4,7 @@ class Juguete(models.Model):
     name = models.CharField(max_length=100)
     year = models.DateField()
     price = models.FloatField()
+    image = models.ImageField(null=True, blank=True)
     class Meta:
         verbose_name = 'juguete'
         verbose_name_plural = 'juguetes'
@@ -12,6 +13,7 @@ class Sucursal(models.Model):
     adress = models.CharField(max_length=200)
     phone = models.IntegerField()
     email = models.CharField(max_length=50)
+    image = models.ImageField(null=True, blank=True)
     class Meta:
         verbose_name = 'sucursal'
         verbose_name_plural = 'sucursales'
@@ -21,6 +23,7 @@ class Empleados(models.Model):
     nombre = models.CharField(max_length=50)
     puesto = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
     class Meta:
         verbose_name = 'empleado'
         verbose_name_plural = 'empleados'
