14607a87a5ab0e143c363ba730489c8e239e9d6d
Mariela Espinoza <mariela.espinozam@gmail.com>
2022-06-08 09:18:41 -0500

    second  changes

--- app_juegos/forms.py
+++ app_juegos/forms.py
@@ -2,29 +2,29 @@ from django import forms
 from app_juegos.models import Juguete,Sucursal,Empleados
 
 class Juguete_form(forms.ModelForm):
-    name = forms.CharField(max_length=100)
-    year = forms.DateField()
-    price = forms.FloatField()
-    image = forms.ImageField()
-    active = forms.BooleanField()
+    # name = forms.CharField(max_length=100)
+    # year = forms.DateField()
+    # price = forms.FloatField()
+    # image = forms.ImageField()
+    # active = forms.BooleanField()
     class Meta:
         model = Juguete
         fields = '__all__'
 
-class Sucursal_form(forms.Form):
-    adress = forms.CharField(max_length=200)
-    phone = forms.IntegerField()
-    email = forms.CharField(max_length=50)
-    image = forms.ImageField()
+class Sucursal_form(forms.ModelForm):
+    # adress = forms.CharField(max_length=200)
+    # phone = forms.IntegerField()
+    # email = forms.CharField(max_length=50)
+    # image = forms.ImageField()
     class Meta:
         model = Sucursal
         fields = '__all__'
 
-class Empleados_form(forms.Form):
-    nombre = forms.CharField(max_length=50)
-    puesto = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    image = forms.ImageField()
+class Empleados_form(forms.ModelForm):
+    # nombre = forms.CharField(max_length=50)
+    # puesto = forms.CharField(max_length=100)
+    # email = forms.CharField(max_length=100)
+    # image = forms.ImageField()
     class Meta:
         model = Empleados
         fields = '__all__'
--- app_juegos/urls.py
+++ app_juegos/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from app_juegos.views import listar_juguetes, crear_juguete_view, crear_sucursal_view, crear_empleado_view
 
 urlpatterns = [
-    path('', listar_juguetes.as_view(), name = 'lista_juguetes'),
-    path('crear-juguete/', crear_juguete.as_view(), name = 'create-juguete'),
-    path('crear-sucursal/', crear_sucursal.as_view(), name = 'create-sucursal'),
-    path('crear-empleado/', crear_empleado.as_view(), name = 'create-empleado'),
+    path('', listar_juguetes, name = 'lista_juguetes'),
+    path('crear-juguete/', crear_juguete_view, name = 'create-juguete'),
+    path('crear-sucursal/', crear_sucursal_view, name = 'create-sucursal'),
+    path('crear-empleado/', crear_empleado_view, name = 'create-empleado'),
 ]
