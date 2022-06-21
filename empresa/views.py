7b246ac21e884a88deb2f753225cbcb483ce29c7
MrMat3000 <mateoingouville@gmail.com>
2022-06-21 19:53:02 -0300

    acceso a CREAR solo si estás logueado

--- empresa/views.py
+++ empresa/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from app_juegos.models import Sucursal, Empleados, Juguete
+from empresa.forms import User_registration_form
 
 def index(request):
     return render(request, 'index.html')
@@ -61,7 +62,7 @@ def logout_view(request):
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = User_registration_form(request.POST)
 
         if form.is_valid():
             form.save()
@@ -73,11 +74,11 @@ def register_view(request):
             return render(request, 'index.html', context = context)
         else:
             errors = form.errors
-            form = UserCreationForm()
+            form = User_registration_form()
             context = {'errors':errors, 'form':form}
             return render(request, 'auth/register.html', context = context)
     else:
-        form = UserCreationForm()
+        form = User_registration_form()
         context = {'form':form}
         return render(request, 'auth/register.html', context =context)
 
