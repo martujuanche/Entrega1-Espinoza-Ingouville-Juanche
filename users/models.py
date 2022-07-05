c4a80137205698efba358b94b5349cd79581ad78
MrMat3000 <mateoingouville@gmail.com>
2022-07-05 18:52:24 -0300

    users

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class User_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='profile_image')
